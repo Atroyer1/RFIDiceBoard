058e7ef1dbe71cd684d48274ce4395f887e94b5a
Atroyer1 <Andrew.troyer72@gmail.com>
2022-10-30 11:08:07 -0700

    PN532 prototyping in progress

--- prototyping/pico-PN532-proto/PN532DriverProto.c
+++ prototyping/pico-PN532-proto/PN532DriverProto.c
@@ -1,22 +1,73 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
-#include "pico/binary_info.h"
-const uint LED_PIN = 25;
-
- int main() {
+#include "hardware/i2c.h"
+#include "PN532DriverProto.h"
 
 
-        stdio_init_all();
+#define I2CADDR 0x24
+#define RST_PIN 2
 
-        gpio_init(LED_PIN);
-        gpio_set_dir(LED_PIN, GPIO_OUT);
-        while (1) {
-            gpio_put(LED_PIN, 0);
-            sleep_ms(250);
-            gpio_put(LED_PIN, 1);
-            puts("Hello World\n");
-            sleep_ms(1000);
-        }
+void dummyRead(void);
+
+int main() {
+//cmdlen:
+//Includes the byte frame identifier (tohost or hostto) as well as
+//the cmd and data bytes
+
+
+    uint8_t cmdlen1 = 2; //cmdlen1 for getfirmwareversion
+                         
+    uint8_t cmdlen2 = 2; //cmdlen2 for return bytes of getfirmwareversion
+    uint8_t checksum1 = (uint8_t)(PN532_PREAMBLE + PN532_STARTCODE1 + PN532_STARTCODE2 + PN532_HOSTTOPN532 + PN532_COMMAND_GETFIRMWAREVERSION); 
+    uint8_t checksum2 = (uint8_t)(PN532_PREAMBLE + PN532_STARTCODE1 + PN532_STARTCODE2 + PN532_PN532TOHOST + (PN532_COMMAND_GETFIRMWAREVERSION + 1));
+    uint8_t cmd1[] = {PN532_PREAMBLE, PN532_STARTCODE1, PN532_STARTCODE2,
+                     cmdlen1, ((~cmdlen1) + 1), PN532_HOSTTOPN532, 
+                     PN532_COMMAND_GETFIRMWAREVERSION,
+                     ~checksum1, PN532_POSTAMBLE
+    };
+    uint8_t cmd2[] = {PN532_PREAMBLE, PN532_STARTCODE1, PN532_STARTCODE2,
+                     cmdlen2, ((~cmdlen2) + 1), PN532_PN532TOHOST, 
+                     PN532_COMMAND_GETFIRMWAREVERSION + 1
+    };
+    uint8_t recieve[32];
+    int ret;
+
+    stdio_init_all();
+
+    gpio_init(RST_PIN);
+    gpio_set_dir(RST_PIN, GPIO_OUT); 
+    //i2c init
+    i2c_init(i2c_default, 100 * 1000);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    
+    gpio_put(RST_PIN, 1);
+    gpio_put(RST_PIN, 0);
+    sleep_ms(400);
+    gpio_put(RST_PIN, 1);
+    sleep_ms(10);
+
+    dummyRead();
+    sleep_ms(100);
+
+    while (1) {
+        i2c_write_blocking(i2c_default, I2CADDR, cmd1, 9, false);
+        sleep_ms(200);
+        //dummyRead();
+        sleep_ms(200);
+        //i2c_write_blocking(i2c_default, I2CADDR, cmd2, 7, false);
+        //i2c_read_blocking(i2c_default, I2CADDR, recieve, 12, false); 
+        while(1){};
+    }
 }
 
+
+void dummyRead(void){
+    uint8_t garbage;
+    i2c_read_blocking(i2c_default, I2CADDR, &garbage, 1, true); 
+}
+
+        //ret = i2c_write_blocking(i2c_default, I2CADDR, &cmd, 1, false);
+        //ret = i2c_read_blocking(i2c_default, I2CADDR, recieve, 12, false);
