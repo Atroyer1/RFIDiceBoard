9a99dee1ce584a448a217328f61cfecc812169d4
Atroyer1 <Andrew.troyer72@gmail.com>
2022-11-07 17:01:52 -0800

    getFirmwareVersion prototype working

--- prototyping/pico-PN532-proto/PN532DriverProto.c
+++ prototyping/pico-PN532-proto/PN532DriverProto.c
@@ -6,8 +6,13 @@
 
 #define I2CADDR 0x24
 #define RST_PIN 2
+#define IRQ_PIN 3
 
 void dummyRead(void);
+void resetTheThing(void);
+uint8_t isReady(uint8_t pin);
+
+uint8_t packetbuffer[64];
 
 int main() {
 //cmdlen:
@@ -36,6 +41,8 @@ int main() {
 
     gpio_init(RST_PIN);
     gpio_set_dir(RST_PIN, GPIO_OUT); 
+    gpio_init(IRQ_PIN);
+    gpio_set_dir(IRQ_PIN, GPIO_IN); 
     //i2c init
     i2c_init(i2c_default, 100 * 1000);
     gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
@@ -49,25 +56,49 @@ int main() {
     gpio_put(RST_PIN, 1);
     sleep_ms(10);
 
-    dummyRead();
-    sleep_ms(100);
-
     while (1) {
         i2c_write_blocking(i2c_default, I2CADDR, cmd1, 9, false);
+        sleep_ms(2);
+        
+        while(isReady(IRQ_PIN)){
+        }
+        
+        i2c_read_blocking(i2c_default, I2CADDR, recieve, 8, false);
+
+        sleep_ms(2);
+
+        while(isReady(IRQ_PIN)){
+        }
+        
+        i2c_read_blocking(i2c_default, I2CADDR, recieve, 14, false);
+        //i2c_read_blocking(i2c_default, I2CADDR, (void), 8, false); 
+        sleep_ms(15);
+        //i2c_read_blocking(i2c_default, I2CADDR, (void), 14, false); 
         sleep_ms(200);
+        
         //dummyRead();
-        sleep_ms(200);
         //i2c_write_blocking(i2c_default, I2CADDR, cmd2, 7, false);
         //i2c_read_blocking(i2c_default, I2CADDR, recieve, 12, false); 
-        while(1){};
+        //while(1){};
     }
 }
 
 
 void dummyRead(void){
     uint8_t garbage;
-    i2c_read_blocking(i2c_default, I2CADDR, &garbage, 1, true); 
+    i2c_read_blocking(i2c_default, I2CADDR, &garbage, 1, false); 
 }
 
         //ret = i2c_write_blocking(i2c_default, I2CADDR, &cmd, 1, false);
         //ret = i2c_read_blocking(i2c_default, I2CADDR, recieve, 12, false);
+
+
+void resetTheThing(void){
+    
+
+
+}
+
+uint8_t isReady(uint8_t pin){
+    gpio_get(IRQ_PIN);
+}
